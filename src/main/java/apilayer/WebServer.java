226923929508fc844eb0e9d3cf59360aaafe1094
celers <celers.dev@gmail.com>
2017-04-19 12:17:04 +0200

    Added Maven Properties

--- src/main/java/apilayer/WebServer.java
+++ src/main/java/apilayer/WebServer.java
@@ -2,28 +2,46 @@ package apilayer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.staticfiles.StaticFilesConfiguration;
 
 import static spark.Spark.*;
 
 public class WebServer {
 
+    private StaticFilesConfiguration staticHandler;
+    private static boolean first = true;
 
     public WebServer() {
         port(Constants.PORT);
+        staticHandler = new StaticFilesConfiguration();
         if (Constants.LOCALHOST) {
             String projectDir = System.getProperty("user.dir");
             String staticDir = "/src/main/resources/public";
-            staticFiles.externalLocation(projectDir + staticDir);
+            staticHandler.configureExternal(projectDir + staticDir);
         } else {
-            staticFiles.location("/public");
+            staticHandler.configure("/public");
         }
+        //staticFileLocation("/public");
         initRouteHandlers();
         initRoutes();
     }
 
     private FBHandler fbHandler;
 
+    private void initRouteHandlers() {
+        fbHandler = new FBHandler();
+    }
+
     private void initRoutes() {
+
+        before((request, response) -> {
+            if (first) {
+                first = false;
+                halt(401);
+            }
+            staticHandler.consume(request.raw(), response.raw());
+        });
+
         get("/a", (request, response) -> "hej");
         post("/fblogin", fbHandler.getFbInfoRequestReceiver()::handle);
         get("/redirect", (request, response) -> {
@@ -31,10 +49,7 @@ public class WebServer {
             logger.info("Request body on redirect " + request.body());
             return request.body();
         });
-    }
 
-    private void initRouteHandlers() {
-        fbHandler = new FBHandler();
     }
 
 }
