48425c331834101c7716441db7734311b13fee42
celers <celers.dev@gmail.com>
2017-04-20 14:38:18 +0200

    login almost done and comments

--- src/main/java/apilayer/handlers/LoginTryHandler.java
+++ src/main/java/apilayer/handlers/LoginTryHandler.java
@@ -1,7 +1,8 @@
 package apilayer.handlers;
 
 import apilayer.RequestHandler;
-import org.hibernate.Criteria;
+import com.google.gson.Gson;
+import model.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -9,8 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
+import utils.Utils;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static spark.Spark.halt;
@@ -19,42 +20,57 @@ public class LoginTryHandler implements RequestHandler {
 
     private static Logger logger = LoggerFactory.getLogger(LoginTryHandler.class);
 
-    private static final String USERNAME_STRING_START = "username=";
-    private static final String PASSWORD_STRING_START = "password=";
-
     private SessionFactory sessionFactory;
 
     public LoginTryHandler(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
+    /** Hanterar en inloggningsrequest
+     *
+     *  Plockar först fram username och password-värdena från <form> som skickas i POST
+     *  Kollar att dessa värden inte är null eller inte är tomma
+     *
+     *  Öppnar sedan en session från hibernate, eftersom det bara är att läsa så behövs ingen transaction
+     *  Skapar en (hibernate) Query som försöker hitta en användare som har
+     *  email = username från form och
+     *  password = password från formen
+     *
+     *  Om resultatet inte är en lista med ett objekt så finns det ingen användare med det användarnamnet
+     *  och lösenordet. (om det är mer än en användare så är det något fel i koden/databasen
+     *
+     *  Finns användaren så skickas ett meddelande om success och en session sätts.
+     *  Om användaren inte finns så skickas ett meddelande om att användaren inte finns
+     *  eller att lösenordet är felaktigt.
+     *
+     *  om username eller password är null eller tomma så skickas error code 500 tillbaka som svar.
+     * */
     @Override
     public Object handle(Request request, Response response) {
-        String body = request.body().trim();
-        String[] split = body.split("&", 3);
-        logger.info(Arrays.toString(split));
-        if (split.length != 2 && containsUsernameAndPassword(split[0], split[1])) {
+        String username = request.queryParams("username");
+        String password = request.queryParams("password");
+        if (Utils.isNotNullAndNotEmpty(username) && Utils.isNotNullAndNotEmpty(password)) {
             try (Session session = sessionFactory.openSession()) {
                 String hql = "FROM User WHERE upper(email) = :email AND password = :password";
                 Query query = session.createQuery(hql);
+                query.setParameter("email", username);
+                query.setParameter("password", password);
                 List result = query.list();
                 if (result.size() != 1) {
-
+                    return new Gson().toJson("{error: 'Felaktigt användarnamn/lösenord')} ");
+                } else {
+                    //set logged in
+                    User user = (User) result.get(0);
+                    request.session(true);
+                    request.session().attribute("user", user);
+                    return new Gson().toJson("{login: 'success'}");
                 }
-
             }
         } else {
             return halt(500);
         }
-        return null;
     }
 
-    /** Returnerar sant om
-     *  requesten som skickas in
-     *
-     * */
-    private boolean containsUsernameAndPassword(String userSplit, String passwordSplit) {
-        return userSplit.startsWith(USERNAME_STRING_START) && passwordSplit.startsWith(PASSWORD_STRING_START);
-    }
+
 
 }
--- src/main/resources/public/js/onLogin.js
+++ src/main/resources/public/js/onLogin.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
 
-    var username_field = $('#username_field');
-    var password_field = $('#password_field');
+    var username_field = $('#username');
+    var password_field = $('#password');
 
     $('#loginform').submit(function (e) {
         e.preventDefault();
-        console.log("username = " + $('#username_field').val());
-        console.log("password = " + $('#password_field').val());
+        // console.log("username = " + username_field.val());
+        // console.log("password = " + password_field.val());
 
-        var data = {username: username_field.val(), password: password_field.val()};
+        var data = {"username": username_field.val(), "password": password_field.val()};
+        console.log("json = " + data);
+        console.log("json = " + data.username);
+        console.log("json = " + data.password);
 
+        $.post('/trylogin', data, function (result) {
+            alert(result);
+        });
 
-        $.ajax({
+        /*$.ajax({
             url: '/trylogin',
             type: "POST",
             data: data,
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function (result) {
-                console.log("Result of login = " + result);
+                alert(result);
             }
 
-        });
+        });*/
 
     });
 
