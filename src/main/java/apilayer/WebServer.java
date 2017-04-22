17a247ccc1cf9404359e35bc1e1c9f90f07b4f46
celers <celers.dev@gmail.com>
2017-04-22 12:56:08 +0200

    test

--- src/main/java/apilayer/WebServer.java
+++ src/main/java/apilayer/WebServer.java
@@ -1,34 +1,48 @@
 package apilayer;
 
 import apilayer.handlers.LoginTryHandler;
+import com.google.gson.Gson;
 import dblayer.HibernateStarter;
 import model.Gender;
 import model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
+import spark.Request;
+import spark.route.RouteOverview;
 import spark.staticfiles.StaticFilesConfiguration;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class WebServer {
 
     private SessionFactory sessionFactory;
+    private Logger logger = LoggerFactory.getLogger(WebServer.class);
 
     public WebServer() {
         port(Constants.PORT);
+        setStaticFilesPath();
+        RouteOverview.enableRouteOverview();
+        initHibernate();
+        initDEVData();
+        initRoutes();
+    }
+
+    private void setStaticFilesPath() {
         if (Constants.LOCALHOST) {
             String projectDir = System.getProperty("user.dir");
             String staticDir = "/src/main/resources/public";
-            staticFiles.externalLocation(projectDir + staticDir);
+            staticFileLocation(projectDir + staticDir);
         } else {
-            staticFiles.location("/public");
+            staticFileLocation("/public");
         }
-        initHibernate();
-        initDEVData();
-        initRouteHandlers();
-        initRoutes();
     }
 
     private void initHibernate() {
@@ -37,6 +51,7 @@ public class WebServer {
             sessionFactory = hibernateStarter.initConfig();
         } catch (Exception e) {
             e.printStackTrace();
+            System.exit(-1);
         }
     }
 
@@ -53,17 +68,33 @@ public class WebServer {
         }
     }
 
-    private FBHandler fbHandler;
 
-    private void initRouteHandlers() {
-        fbHandler = new FBHandler();
-    }
 
     private void initRoutes() {
-        get("/a", (request, response) -> "hej");
+
+        get("/index.html", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.vm");
+        }, new VelocityTemplateEngine());
 
         //Hanterar inloggningsförsök
         post("/trylogin", new LoginTryHandler(sessionFactory)::handle);
+    }
+
+    private boolean isLoggedIn(Request request) {
+        return request.session().attribute("user") != null;
+    }
+
+    private void protectedAPI() {
+        get("/getusername", (request, response) -> {
+            User user = request.session().attribute("user");
+            if (user == null) {
+                logger.info("user is null and not logged in");
+            } else {
+                logger.info(user.getName());
+            }
+            return new Gson().toJson(user);
+        });
     }
 
 }
