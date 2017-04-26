3260d7bc3ba7404690cb3bcc64971b4ea35dbd13
celers <celers.dev@gmail.com>
2017-04-26 10:25:22 +0200

    inte progress

---
+++ src/main/java/stockholmapi/ApiLoader.java
@@ -0,0 +1,99 @@
+package stockholmapi;
+
+import com.google.gson.Gson;
+import com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import org.w3c.dom.Document;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ApiLoader {
+
+    public static void main(String[] args) throws Exception{
+        final String API_KEY = "a42963ca81a64e55869481b281ad36c0";
+        final String BASE_API_URL = "http://api.stockholm.se";
+        String serviceUnits = "/ServiceGuideService/ServiceUnitTypes/9da341e4-bdc6-4b51-9563-e65ddc2f7434/ServiceUnits?apikey=";
+        String url = BASE_API_URL + serviceUnits + API_KEY;
+        System.out.println(url);
+        //String xml = getUrl(url);
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(new URL(url).openStream());
+        printDocument(document,System.out);
+
+
+
+    }
+
+    public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        transformer.transform(new DOMSource(doc),
+                new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+    }
+
+
+    public static String getUrl(String urlStr) throws Exception {
+        URL url = new URL(urlStr);
+        URLConnection urlConnection = url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        bufferedReader.close();
+        return stringBuilder.toString();
+    }
+
+    class ServiceUnitTypes {
+        String Name, TimeUpdated, TimeCreated, Id;
+        GeographicalPosition GeographicalPosition;
+
+        @Override
+        public String toString() {
+            return "ServiceUnitTypes{" +
+                    "Name='" + Name + '\'' +
+                    ", Timeupdated='" + TimeUpdated + '\'' +
+                    ", TimeCreated='" + TimeCreated + '\'' +
+                    ", Id='" + Id + '\'' +
+                    ", GeographicalPosition=" + GeographicalPosition +
+                    '}';
+        }
+    }
+
+    class GeographicalPosition {
+        long X, Y;
+
+        @Override
+        public String toString() {
+            return "GeographicalPosition{" +
+                    "X=" + X +
+                    ", Y=" + Y +
+                    '}';
+        }
+    }
+
+
+
+}
