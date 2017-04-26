34aadb171500e0a45c6c9e61ebc86905144163f0
celers <celers.dev@gmail.com>
2017-04-26 14:14:07 +0200

    :)

--- src/main/java/apilayer/handlers/AuthChecker.java
+++ src/main/java/apilayer/handlers/AuthChecker.java
@@ -8,6 +8,13 @@ import spark.Response;
 @Slf4j
 public class AuthChecker {
 
+
+    /** Metoden returnerar true om användaren är inloggad
+     *  loggar användarens ip på info
+     * @param request request-objektet där session kan hämtas ifrån.
+     * @param response ingen användning för tillfället
+     * @return om användaren är inloggad
+     * */
     public static boolean isLoggedIn(Request request, Response response) {
         log.info("client " + request.ip() + " checking loginstatus = " + request.session().attribute("user"));
         return request.session().attribute(Constants.USER_SESSION_KEY) != null;
