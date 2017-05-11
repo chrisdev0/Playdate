f6a6bd3a286f4df0d807e4d0f326d51c0f213547
celers <celers.dev@gmail.com>
2017-05-11 21:39:34 +0200

    mockito start

---
+++ src/test/java/util/MockTestHelpers.java
@@ -0,0 +1,53 @@
+package util;
+
+import apilayer.Constants;
+import model.User;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import java.util.HashSet;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MockTestHelpers {
+
+
+    public Request initRequestMock(User user) {
+        Request request = mock(Request.class);
+        Session session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        when(session.attribute(Constants.USER_SESSION_KEY)).thenReturn(user);
+        when(request.session().attributes()).thenReturn(new HashSet<>());
+
+        return request;
+    }
+
+    public Response initResponseMock() {
+        return mock(Response.class);
+    }
+
+
+    public void injectKeyValues(Request request, KeyValue... keyValues) {
+        for (KeyValue e : keyValues) {
+            when(request.queryParams(e.key)).thenReturn(e.value);
+        }
+    }
+
+    public void injectSingleKeyValue(Request request, String key, String value) {
+        when(request.queryParams(key)).thenReturn(value);
+    }
+
+
+    public class KeyValue {
+        String key, value;
+
+        public KeyValue(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+}
