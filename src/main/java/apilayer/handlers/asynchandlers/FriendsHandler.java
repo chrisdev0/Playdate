24ba552b9a5e1417264a871e35a5217a00b12479
celers <celers.dev@gmail.com>
2017-05-25 12:10:54 +0200

    fixa friends

--- src/main/java/apilayer/handlers/asynchandlers/FriendsHandler.java
+++ src/main/java/apilayer/handlers/asynchandlers/FriendsHandler.java
@@ -18,31 +18,22 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static apilayer.handlers.asynchandlers.SparkHelper.getOffsetFromRequest;
-import static apilayer.handlers.asynchandlers.SparkHelper.getUserFromSession;
-import static apilayer.handlers.asynchandlers.SparkHelper.setStatusCodeAndReturnString;
+import static apilayer.handlers.asynchandlers.SparkHelper.*;
 
 @Slf4j
 public class FriendsHandler {
 
     public static Object handleAcceptFriendRequest(Request request, Response response) {
-        String userIdStr = request.queryParams(Paths.QueryParams.USER_BY_ID);
-        Long userId = ParserHelpers.parseToLong(userIdStr);
-        User user = request.session().attribute(Constants.USER_SESSION_KEY);
-        Optional<User> userById = UserDAO.getInstance().getUserById(userId);
+        Optional<User> userById = getUserFromRequestById(request);
         if (userById.isPresent()) {
-            Optional<FriendshipRequest> friendshipRequest = UserDAO.getInstance().checkIfFriendRequestSent(user.getId(), userById.get().getId());
+            Optional<FriendshipRequest> friendshipRequest = UserDAO.getInstance().checkIfFriendRequestSent(getUserFromSession(request).getId(), userById.get().getId());
             if (friendshipRequest.isPresent()) {
-                FriendshipRequest fr = friendshipRequest.get();
-                Optional<Friendship> friendship = UserDAO.getInstance().createFriendship(fr);
-                if (friendship.isPresent()) {
-                    response.status(200);
-                    return "";
+                if (UserDAO.getInstance().createFriendship(friendshipRequest.get()).isPresent()) {
+                    return setStatusCodeAndReturnString(response, 200, Constants.MSG.OK);
                 }
             }
         }
-        response.status(400);
-        return "";
+        return setStatusCodeAndReturnString(response, 200, Constants.MSG.ERROR);
     }
 
     public static Object handleDeclineFriendshipRequest(Request request, Response response) {
